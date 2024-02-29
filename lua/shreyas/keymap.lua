b42fa4c2f74d4d3775957127270860d38855012f
Shreyas Mididoddi <me@shreyascodes.tech>
2024-02-29 23:48:54 +0530

    update paste keymaps

--- lua/shreyas/keymap.lua
+++ lua/shreyas/keymap.lua
@@ -14,8 +14,8 @@ xmap("K", ":m '<-2<CR>gv=gv", "Move Selection up")
 
 nmap("U", "<C-r>", "Redo")
 
-xmap("<leader>p", [["+p]], "Paste from clipboard")
-xmap("<leader>P", [["+P]], "Paste from clipboard before cursor")
+nmap("<leader>p", [["+p]], "Paste from clipboard")
+nmap("<leader>P", [["+P]], "Paste from clipboard before cursor")
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to clipboard" })
 nmap("<leader>Y", [["+Y]], "Copy to clipboard until end of line")
 nmap("<leader>e", vim.cmd.Ex, "Open Netrw")
