cbb816bad7d95ddaaf5561f8179ad0471f54d79f
Shreyas Mididoddi <me@shreyascodes.tech>
2024-04-16 05:14:35 -0400

    changes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,6 +9,16 @@ return {
 			section_separators = { left = "", right = "" },
 		},
 		sections = {
+			lualine_a = {
+				function()
+					local mode = require("lualine.utils.mode").get_mode()
+					local recording = vim.fn.reg_recording()
+					if recording == "" then
+						return mode
+					end
+					return " " .. recording
+				end,
+			},
 			lualine_c = { {
 				"buffers",
 				filetype_names = {
