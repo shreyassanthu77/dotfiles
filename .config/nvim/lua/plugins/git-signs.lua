d8315a2d90f2210f974f40e49d49a5bbadab59a0
Shreyas Mididoddi <me@shreyascodes.tech>
2024-06-15 15:14:54 +0530

    nvim: git line blames ftw

--- .config/nvim/lua/plugins/git-signs.lua
+++ .config/nvim/lua/plugins/git-signs.lua
@@ -1,6 +1,9 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-		require("gitsigns").setup()
+		local gitsigns = require("gitsigns")
+		gitsigns.setup({
+			current_line_blame = true,
+		})
 	end
 }
