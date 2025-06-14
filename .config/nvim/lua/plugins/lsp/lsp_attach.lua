42ee95b86a975ff940ad37b217b9c426b97867d0
Shreyas Mididoddi <me@shreyascodes.tech>
2025-06-15 03:50:11 +0530

    nvim: use builtin lsp instead of telescope for go to def

--- .config/nvim/lua/plugins/lsp/lsp_attach.lua
+++ .config/nvim/lua/plugins/lsp/lsp_attach.lua
@@ -35,8 +35,8 @@ local function on_attach(client, bufnr)
 	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
 
-	nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-	-- nmap("g", vim.lsp.buf.definition, "[G]oto [D]efinition")
+	-- nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 	nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 	nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
